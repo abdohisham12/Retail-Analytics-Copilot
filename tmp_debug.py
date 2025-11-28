224c18a515d096f944f236c8d5ea6ce86d513f29
abdohisham12 <abdulrahmanhishamk@gmail.com>
2025-11-28 17:44:28 +0200

    clean the repo to match the skeleton requirment

--- tmp_debug.py
+++
@@ -1,6 +0,0 @@
-from agent.graph_hybrid import _build_fallback_sql
-from agent.tools.sqlite_tool import SQLiteTool
-sql = _build_fallback_sql('hybrid_top_category_qty_summer_1997', SQLiteTool(), {'question': 'During Summer Beverages 1997 ...'})
-with open('debug_output.txt', 'w', encoding='utf-8') as f:
-    f.write(f'sql is None? {sql is None}\n')
-    f.write(str(sql))
